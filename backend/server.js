426131452138a03f1a92cd5a4b4714afa834791c
Fikayo Adepoju <fikayo@Fikayos-MacBook-Pro.local>
2024-03-04 23:02:06 +0100

    Captions working

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const {GoogleGenerativeAI} = require("@google/generative-ai");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const multer  = require('multer');
 const path    = require('path');
 const fs = require("fs");
-const mime = require("mime/lite");
+//const mime = require("mime/lite");
+const mime = require('mime-types');
+
+dotenv.config();
 
 const app = express();
 
@@ -36,8 +40,10 @@ app.get("/", (req, res) => {
 
 app.post("/caption-image", upload.single('file'), async (req, res) => {
 
+    console.log(process.env.API_KEY);
+
     const filePath = req.file.path;
-    const mimeType = mime.getType(filePath);
+    const mimeType = mime.lookup(filePath);
 
     const imagePath = {
         inlineData: {
@@ -55,6 +61,7 @@ app.post("/caption-image", upload.single('file'), async (req, res) => {
     const request = await geminiProModel.generateContent([prompt, ...images]);
     const response = await request.response;
     const caption = response.text();
+    console.log(response);
     console.log(caption);
 
     res.send(caption);
